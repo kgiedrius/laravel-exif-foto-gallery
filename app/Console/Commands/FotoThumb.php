c3750e1b713ccc056ed8e2d19eb912e279dcffc9
giedrius <kgiedrius@gmail.com>
2016-06-18 01:01:47 +0300

    updates

--- app/Console/Commands/FotoThumb.php
+++ app/Console/Commands/FotoThumb.php
@@ -6,6 +6,7 @@ use App\Image;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
