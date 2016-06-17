e300912423609a843fcb3dae6889fc5a059ea63a
giedrius <kgiedrius@gmail.com>
2016-06-18 01:01:16 +0300

    updates

--- app/Console/Commands/FotoThumb.php
+++ app/Console/Commands/FotoThumb.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
 
 class FotoThumb extends Command
 {
