2812839b7cba4896afd2e498f107120b539399de
giedrius <kgiedrius@gmail.com>
2016-06-17 22:46:59 +0300

    updates

--- app/Console/Commands/DeleteDuplicates.php
+++ app/Console/Commands/DeleteDuplicates.php
@@ -48,7 +48,7 @@ class DeleteDuplicates extends Command
             echo $file . "\n";
 
             Image::destroy($record->id);
-            File::delete($file);
+            File::delete('"'.$file.'"');
             //unlink( '"'.$file.'"' );
         }
         //   DB::Select()
