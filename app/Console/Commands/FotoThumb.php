9ed587b517e98241db87e987bcfd77fd538ed69a
giedrius <kgiedrius@gmail.com>
2016-06-18 00:59:02 +0300

    .

---
+++ app/Console/Commands/FotoThumb.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Image;
+use Illuminate\Console\Command;
+
+class FotoThumb extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'foto:thumb';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate Thumbs';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $thumbPath = Config::get( 'images.thumb_path' );
+        $size = '300x200';
+
+        $photoList = DB::select( 'select * from images order by id asc limit 5' );
+        $cnt = 0;
+        $totalCount = Image::count();
+        foreach ( $photoList as $record ) {
+            $destinationFile = $record->file_hash . ".$size" . '.jpg';
+            $destinationDir = $thumbPath . '/' . substr( $record->file_hash, 0, 2 ).'/';
+            mkdir($destinationDir,0777,true);
+            $destination = $destinationDir . $destinationFile;
+            $cnt++;
+
+            echo round( ( $cnt / $totalCount ) * 100, 3 ) . '%: ' . $destination . "\n";
+            $destination = '"' . $destination . '"';
+            $source = '"' . $record->path . '/' . $record->file_name . '"';
+
+            exec( "convert $source -resize 300x200^ -gravity center -crop 300x200+0+0 +repage $destination " );
+        }
+    }
+}
